d365309801c715fa1f679b67dc6bb0edd79c4490
SkyBuilder1717 <skybuilderoffical@yandex.ru>
2024-09-01 16:12:27 +0300

    Unifieddyes support

--- init.lua
+++ init.lua
@@ -2,36 +2,16 @@ local modname = minetest.get_current_modname()
 local modpath = minetest.get_modpath(modname)
 local S = minetest.get_translator(modname)
 chromakey = {
-    registered_chromakeys = {},
-	colors = {
-		{"white",      "White"},
-		{"grey",       "Grey"},
-		{"dark_grey",  "Dark Grey"},
-		{"black",      "Black"},
-		{"violet",     "Violet"},
-		{"blue",       "Blue"},
-		{"dark_cyan",  "Dark Cyan"},
-		{"cyan",       "Cyan"},
-		{"navajo_white","Navajo White"},
-		{"dark_green", "Dark Green"},
-		{"green",      "Green"},
-		{"medium_spring_green", "Medium Spring Green"},
-		{"lime",       "Lime"},
-		{"yellow",     "Yellow"},
-		{"olive",      "Olive"},
-		{"brown",      "Brown"},
-		{"orange",     "Orange"},
-		{"red",        "Red"},
-		{"magenta",    "Magenta"},
-		{"pink",       "Pink"},
-	}
+    registered_chromakeys = {}
 }
-dofile(modpath.."/api.lua")
 
-for _, row in ipairs(chromakey.colors) do
-	local name = row[1]
-	local description = row[2]
-	chromakey.register_block("chromakey:"..name, {
-		color = S(description)
-	})
-end
+minetest.register_node("chromakey:chroma", {
+	description = S("Chroma block"),
+	tiles = {"chromakey_white.png"},
+	groups = {cracky = 1, ud_param2_colorable = 1},
+	paramtype = "light",
+	paramtype2 = "color",
+	palette = "unifieddyes_palette_extended.png",
+	light_source = minetest.LIGHT_MAX,
+	on_dig = unifieddyes.on_dig
+})
